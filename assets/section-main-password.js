d7999e8deb304792a93491943b1fa02fe06260d4
tangkete <cott@shopline.com>
2024-08-26 14:35:57 +0800

    init dev

--- assets/section-main-password.js
+++
@@ -1,9 +0,0 @@
-class PasswordModal extends DetailsModal {
-  connectedCallback() {
-    super.connectedCallback?.();
-    // The initial opening of the pop-up page works on the error form
-    if (this.querySelector('input[data-error="true"]')) this.open({ target: this.querySelector('details') });
-  }
-}
-
-customElements.define('password-modal', PasswordModal);
